023712bef0eb36c6436453976db511c1571de66e
Ayoub GTC <ayoubsnini@gmail.com>
2023-12-04 16:12:00 +0100

    start

---
+++ FrontEnd & Backend/connexion.php
@@ -0,0 +1,25 @@
+ <?php
+
+$servername = "localhost";
+$database = "dataware_v2";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+
+
+// Close connection
+// mysqli_close($conn);
+
+
+
+
+
+?>
