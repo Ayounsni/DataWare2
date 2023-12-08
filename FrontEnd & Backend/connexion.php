7a4d551de66662f617ac883e8d9753e2211c828a
nabilettihadi <nabilettihadi.ma@gmail.com>
2023-12-08 19:08:43 +0100

    DataWare

--- FrontEnd & Backend/connexion.php
+++ FrontEnd & Backend/connexion.php
@@ -10,6 +10,16 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+
+
+
+
 // Close connection
 // mysqli_close($conn);
+
+
+
+
+
 ?>
